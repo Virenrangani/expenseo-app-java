40b54f5a0e3421892ef407fdd669434b2a37631c
Virenrangani <virenrangani7@gmail.com>
2026-05-10 09:37:41 +0530

    refactor: make instance variable final

--- src/main/java/com/example/expenseo/service/UserService.java
+++ src/main/java/com/example/expenseo/service/UserService.java
@@ -1,19 +1,21 @@
-package com.example.expenseo.service;
+    package com.example.expenseo.service;
 
-import com.example.expenseo.dto.UserRequest;
-import com.example.expenseo.dto.UserResponse;
-import com.example.expenseo.mapper.UserMapStructMapper;
-import com.example.expenseo.models.UserModel;
-import com.example.expenseo.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+    import com.example.expenseo.dto.UserRequest;
+    import com.example.expenseo.dto.UserResponse;
+    import com.example.expenseo.mapper.UserMapStructMapper;
+    import com.example.expenseo.models.UserModel;
+    import com.example.expenseo.repository.UserRepository;
+    import lombok.AllArgsConstructor;
+    import lombok.NoArgsConstructor;
+    import lombok.RequiredArgsConstructor;
+    import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-    private UserRepository userRepository;
-    private UserMapStructMapper userMapper;
+    private final UserRepository userRepository;
+    private final UserMapStructMapper userMapper;
 
 
     public UserResponse signUp( UserRequest user ) {
