6db00deb07e3148c67d40d55fb1a2d5c91107370
Virenrangani <virenrangani7@gmail.com>
2026-05-11 07:34:21 +0530

    feat: add method that find the email of user

--- src/main/java/com/example/expenseo/repository/UserRepository.java
+++ src/main/java/com/example/expenseo/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.example.expenseo.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserModel,String> {
 
     boolean existsByEmail(String email);
+
+    Optional<UserModel> findByEmail(String email);
 }
