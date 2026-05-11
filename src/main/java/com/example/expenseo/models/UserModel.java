b164572493f7d85b2084a783b54fba49df66847f
Virenrangani <virenrangani7@gmail.com>
2026-05-11 07:33:26 +0530

    feat: add implementation of userDetails interface and its method

--- src/main/java/com/example/expenseo/models/UserModel.java
+++ src/main/java/com/example/expenseo/models/UserModel.java
@@ -4,6 +4,12 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
 
 import java.time.LocalDateTime;
 
@@ -14,7 +20,8 @@ import java.time.LocalDateTime;
 @Setter
 @Builder
 @Table(name = "users")
-public class UserModel {
+
+public class UserModel implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -35,4 +42,28 @@ public class UserModel {
 
     @UpdateTimestamp
     private  LocalDateTime updatedAt;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        // For now, we will give everyone a standard "USER" role.
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        // Spring uses "username" for login, but we are using email!
+        return email;
+    }
+
+    // These boolean methods are for account locking/expiring.
+    // In production, just return true unless you build features to ban users.
+    @Override
+    public boolean isAccountNonExpired() { return true; }
+    @Override
+    public boolean isAccountNonLocked() { return true; }
+    @Override
+    public boolean isCredentialsNonExpired() { return true; }
+    @Override
+    public boolean isEnabled() { return true; }
 }
