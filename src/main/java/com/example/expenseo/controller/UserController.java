1acff211c4ab355cc0a6b27fec76f4fa1531bd37
Virenrangani <virenrangani7@gmail.com>
2026-05-12 06:57:34 +0530

    feat: add login method

--- src/main/java/com/example/expenseo/controller/UserController.java
+++ src/main/java/com/example/expenseo/controller/UserController.java
@@ -1,7 +1,8 @@
 package com.example.expenseo.controller;
 
+import com.example.expenseo.dto.AuthResponse;
+import com.example.expenseo.dto.LoginRequest;
 import com.example.expenseo.dto.UserRequest;
-import com.example.expenseo.dto.UserResponse;
 import com.example.expenseo.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -20,10 +21,16 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<UserResponse> signUp(@RequestBody @Valid UserRequest user){
+    public ResponseEntity<AuthResponse> signUp(@RequestBody @Valid UserRequest user){
 
-        UserResponse response=userService.signUp(user);
+        AuthResponse response=userService.signUp(user);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request){
+        AuthResponse response = userService.login(request);
+        return ResponseEntity.ok(response);
     }
 }
