3fa92730b10c096740e7b3e184ead4c626ee8efe
viren <virenrangani7@gmail.com>
2026-05-13 14:52:36 +0530

    feat: create endpoints verify

--- src/main/java/com/example/expenseo/controller/UserController.java
+++ src/main/java/com/example/expenseo/controller/UserController.java
@@ -8,10 +8,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -32,5 +29,11 @@ public class UserController {
     public ResponseEntity<AuthResponse> login(@Valid @RequestBody LoginRequest request){
         AuthResponse response = userService.login(request);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/verify")
+    public ResponseEntity<String> verifyEmail(@RequestParam String token){
+        userService.verifyEmail(token);
+        return ResponseEntity.ok("Email successfully verified! You can now log in.");
     }
 }
