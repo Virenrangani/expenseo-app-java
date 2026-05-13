f103286513007fc12a3375837569ba05d8b3009c
viren <virenrangani7@gmail.com>
2026-05-13 14:49:27 +0530

    feat: add VerificationToken Repository to find token from database

---
+++ src/main/java/com/example/expenseo/repository/VerificationTokenRepository.java
@@ -0,0 +1,12 @@
+package com.example.expenseo.repository;
+
+import com.example.expenseo.models.VerificationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken , Long> {
+    Optional<VerificationToken> findByToken(String token);
+}
