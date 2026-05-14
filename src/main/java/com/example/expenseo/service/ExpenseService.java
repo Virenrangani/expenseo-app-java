d6e0f195d35951953bf40fde847d63a3780d2f42
viren <virenrangani7@gmail.com>
2026-05-14 14:51:59 +0530

    feat: add getAllExpense method

--- src/main/java/com/example/expenseo/service/ExpenseService.java
+++ src/main/java/com/example/expenseo/service/ExpenseService.java
@@ -12,7 +12,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -24,25 +26,37 @@ public class ExpenseService {
     public ExpenseResponse createExpense(ExpenseRequest request){
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
         if (authentication == null || !authentication.isAuthenticated()) {
             // In a real app, you might throw a custom UnauthorizedException here
             throw new RuntimeException("Authentication context is missing or invalid.");
         }
 
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-
         UserModel currentUser = userDetails.getUser();
 
         // 2. Map the Request DTO to the Entity
-
         ExpenseModel newExpense = expenseMapper.toEntity(request);
-
         newExpense.setUser(currentUser);
 
         ExpenseModel saveExpense = expenseRepository.save(newExpense);
 
         return expenseMapper.toResponse(saveExpense);
+    }
 
+    public List<ExpenseResponse> getAllExpenses(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()){
+            throw new RuntimeException("Authentication context is missing or invalid.");
+        }
+
+        CustomUserDetails currentUser = (CustomUserDetails) authentication.getPrincipal();
+        String userId = currentUser.getUser().getId();
+
+        List<ExpenseModel> savedExpenses = expenseRepository
+                .findByUserIdOrderByExpenseDateDesc(userId);
+
+        return savedExpenses.stream()
+                .map(expenseMapper::toResponse)
+                .collect(Collectors.toList());
     }
 }
