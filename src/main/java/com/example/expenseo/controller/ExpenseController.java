57ad3351686aa1d9bf381f1c53ed7134c20e7b85
viren <virenrangani7@gmail.com>
2026-05-14 14:52:22 +0530

    feat: add getAllExpense method with get mapping

--- src/main/java/com/example/expenseo/controller/ExpenseController.java
+++ src/main/java/com/example/expenseo/controller/ExpenseController.java
@@ -2,15 +2,15 @@ package com.example.expenseo.controller;
 
 import com.example.expenseo.dto.ExpenseRequest;
 import com.example.expenseo.dto.ExpenseResponse;
+import com.example.expenseo.models.ExpenseModel;
 import com.example.expenseo.service.ExpenseService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +24,11 @@ public class ExpenseController {
             ){
         ExpenseResponse response = expenseService.createExpense(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<ExpenseResponse>> getAllExpense(){
+        List<ExpenseResponse> responses = expenseService.getAllExpenses();
+        return ResponseEntity.ok(responses);
     }
 }
