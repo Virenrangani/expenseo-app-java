bfe1e68d769af915afe9d1d7f65d7d42669dc8b3
viren <virenrangani7@gmail.com>
2026-05-14 14:51:32 +0530

    feat: add findByUserIdOrderByExpenseDateDesc method then return all the expenses

--- src/main/java/com/example/expenseo/repository/ExpenseRepository.java
+++ src/main/java/com/example/expenseo/repository/ExpenseRepository.java
@@ -3,5 +3,9 @@ package com.example.expenseo.repository;
 import com.example.expenseo.models.ExpenseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExpenseRepository extends JpaRepository<ExpenseModel, String> {
+
+    List<ExpenseModel> findByUserIdOrderByExpenseDateDesc(String userId);
 }
