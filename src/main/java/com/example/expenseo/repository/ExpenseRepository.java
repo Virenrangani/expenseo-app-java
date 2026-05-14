8b1988420e1a25d7c25e44339caa1ce08986a3e8
Virenrangani <virenrangani7@gmail.com>
2026-05-14 21:52:15 +0530

    feat: add findByAndUserId method

--- src/main/java/com/example/expenseo/repository/ExpenseRepository.java
+++ src/main/java/com/example/expenseo/repository/ExpenseRepository.java
@@ -4,8 +4,11 @@ import com.example.expenseo.models.ExpenseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExpenseRepository extends JpaRepository<ExpenseModel, String> {
 
     List<ExpenseModel> findByUserIdOrderByExpenseDateDesc(String userId);
+
+    Optional<ExpenseModel> findByIdAndUserId(String id , String userId);
 }
