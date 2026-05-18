fb4c15240a713a32245a03407b3f3c06c7253404
Virenrangani <virenrangani7@gmail.com>
2026-05-18 20:32:26 +0530

    feat: add repository annotation

--- src/main/java/com/example/expenseo/repository/ExpenseRepository.java
+++ src/main/java/com/example/expenseo/repository/ExpenseRepository.java
@@ -2,10 +2,12 @@ package com.example.expenseo.repository;
 
 import com.example.expenseo.models.ExpenseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ExpenseRepository extends JpaRepository<ExpenseModel, String> {
 
     List<ExpenseModel> findByUserIdOrderByExpenseDateDesc(String userId);
