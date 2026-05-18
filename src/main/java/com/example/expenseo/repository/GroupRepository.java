ac2c926ea023c1074d005d5c2ed19c0cee379d97
viren <virenrangani7@gmail.com>
2026-05-18 14:30:19 +0530

    feat: add method findByMembersId OrderByCreatedAtDesc

--- src/main/java/com/example/expenseo/repository/GroupRepository.java
+++ src/main/java/com/example/expenseo/repository/GroupRepository.java
@@ -4,7 +4,10 @@ import com.example.expenseo.models.GroupModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GroupRepository extends JpaRepository<GroupModel , String> {
 
+    List<GroupModel> findByMembersIdOrderByCreatedAtDesc(String userId);
 }
