9659d712463ad16c270943716d767a9f5ef435a6
viren <virenrangani7@gmail.com>
2026-05-18 09:42:44 +0530

    feat: add Group controller with create Group post mapping

---
+++ src/main/java/com/example/expenseo/controller/GroupController.java
@@ -0,0 +1,27 @@
+package com.example.expenseo.controller;
+
+import com.example.expenseo.dto.GroupRequest;
+import com.example.expenseo.dto.GroupResponse;
+import com.example.expenseo.service.GroupService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/group")
+public class GroupController {
+
+    private final GroupService groupService;
+
+    @PostMapping
+    public ResponseEntity<GroupResponse> createGroup(@Valid @RequestBody GroupRequest request){
+        GroupResponse response = groupService.createGroup(request);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
