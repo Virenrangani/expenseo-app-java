b162e87a660d45aab3cdd7d9ef7dc9ac27795ef1
viren <virenrangani7@gmail.com>
2026-05-19 18:16:30 +0530

    feat: add getGroupExpense with GetMapping

--- src/main/java/com/example/expenseo/controller/GroupExpenseController.java
+++ src/main/java/com/example/expenseo/controller/GroupExpenseController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +24,12 @@ public class GroupExpenseController {
 
         GroupExpenseResponse response = groupExpenseService.createGroupExpense(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("{groupId}")
+    public ResponseEntity<List<GroupExpenseResponse>> getGroupExpense(@PathVariable String groupId){
+        List<GroupExpenseResponse> expense = groupExpenseService.getGroupExpense(groupId);
+
+        return ResponseEntity.ok(expense);
     }
 }
