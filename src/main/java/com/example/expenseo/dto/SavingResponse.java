4650d046b6a403aa38b6613207bdaee53f355869
viren <virenrangani7@gmail.com>
2026-05-28 14:42:08 +0530

    feat: add Saving response dto class

---
+++ src/main/java/com/example/expenseo/dto/SavingResponse.java
@@ -0,0 +1,30 @@
+package com.example.expenseo.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SavingResponse {
+
+    private String id;
+
+    private String goal;
+
+    private BigDecimal targetAmount;
+
+    private BigDecimal savingAmount = BigDecimal.ZERO;
+
+    private Boolean isCompleted = false;
+
+    private LocalDateTime createdAt;
+
+    private String  userId;
+}
