b2195236e3afaf3d03f668f34277fc8c621d7b3c
viren <virenrangani7@gmail.com>
2026-05-28 14:42:32 +0530

    feat: add saving mapper mapping interface

---
+++ src/main/java/com/example/expenseo/mapper/SavingMapper.java
@@ -0,0 +1,18 @@
+package com.example.expenseo.mapper;
+
+import com.example.expenseo.dto.SavingRequest;
+import com.example.expenseo.dto.SavingResponse;
+import com.example.expenseo.models.SavingModel;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "string" ,builder = @Builder(disableBuilder = true))
+public interface SavingMapper {
+
+    SavingModel toRequest(SavingRequest request);
+
+    @Mapping(source = "user.id" , target = "userId")
+    SavingResponse toResponse(SavingModel saving);
+
+}
