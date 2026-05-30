1d295ec022124ec014ecf11c443c99f0cbd1b402
Virenrangani <virenrangani7@gmail.com>
2026-05-30 12:48:12 +0530

    feat: add findBySavingGoalIdOrderByCreatedAtDesc method with goalId

--- src/main/java/com/example/expenseo/repository/DepositRepository.java
+++ src/main/java/com/example/expenseo/repository/DepositRepository.java
@@ -4,6 +4,9 @@ import com.example.expenseo.models.DepositModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DepositRepository extends JpaRepository<DepositModel,String> {
+    List<DepositModel> findBySavingGoalIdOrderByCreatedAtDesc(String goalId);
 }
