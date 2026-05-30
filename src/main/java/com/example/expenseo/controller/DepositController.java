25136a70939933a37600d2c5b5f88d3b203ba97e
Virenrangani <virenrangani7@gmail.com>
2026-05-30 12:52:17 +0530

    feat: add end points of get mapping using getAllDeposit

--- src/main/java/com/example/expenseo/controller/DepositController.java
+++ src/main/java/com/example/expenseo/controller/DepositController.java
@@ -1,16 +1,16 @@
 package com.example.expenseo.controller;
 
 import com.example.expenseo.dto.DepositRequest;
+import com.example.expenseo.dto.DepositResponse;
 import com.example.expenseo.dto.SavingResponse;
 import com.example.expenseo.service.DepositService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +23,12 @@ public class DepositController {
     public ResponseEntity<SavingResponse> addGoalDeposit(@Valid @RequestBody DepositRequest request){
         SavingResponse response = depositService.addGoalDeposit(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/deposit/{goalId}")
+    public ResponseEntity<List<DepositResponse>> getAllDeposit(@PathVariable String goalId){
+        List<DepositResponse> responses =depositService.getAllDeposit(goalId);
+
+        return ResponseEntity.ok(responses);
     }
 }
