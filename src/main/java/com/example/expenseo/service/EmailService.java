c288b8a0c1b9ceadb72a19d3661bc7b2967660ae
Virenrangani <virenrangani7@gmail.com>
2026-06-10 08:18:53 +0530

    feat: add sendPasswordResetEmail method

--- src/main/java/com/example/expenseo/service/EmailService.java
+++ src/main/java/com/example/expenseo/service/EmailService.java
@@ -22,4 +22,17 @@ public class EmailService {
 
         mailSender.send(message);
     }
+
+
+    public void sendPasswordResetEmail(String toEmail, String otp) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setSubject("Expenseo - Password Reset Code");
+
+        message.setText("We received a request to reset your password.\n\n" +
+                "Your password reset code is: " + otp + "\n\n" +
+                "This code will expire in 10 minutes. If you did not request this, please ignore this email.");
+
+        mailSender.send(message);
+    }
 }
