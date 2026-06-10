34d5e6e3034b211fd0bb08be8b128654838c9e05
viren <virenrangani7@gmail.com>
2026-06-10 14:20:16 +0530

    feat: add Reset Password Request dto class

---
+++ src/main/java/com/example/expenseo/dto/ResetPasswordRequest.java
@@ -0,0 +1,24 @@
+package com.example.expenseo.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordRequest {
+
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "OTP is required")
+    private String otp;
+
+    @NotBlank(message = "New password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String newPassword;
+
+}
