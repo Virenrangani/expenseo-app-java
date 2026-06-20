338478bfe04f548ab33db8f2866ace714230da75
Virenrangani <virenrangani7@gmail.com>
2026-06-20 07:36:25 +0530

    feat: add Mapping in toResponse for createdAt

--- src/main/java/com/example/expenseo/mapper/ExpenseMapper.java
+++ src/main/java/com/example/expenseo/mapper/ExpenseMapper.java
@@ -4,6 +4,7 @@ import com.example.expenseo.dto.ExpenseRequest;
 import com.example.expenseo.dto.ExpenseResponse;
 import com.example.expenseo.models.ExpenseModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
@@ -11,5 +12,6 @@ public interface ExpenseMapper {
 
     ExpenseModel toEntity (ExpenseRequest request);
 
+    @Mapping(source = "expenseDate", target = "createdAt")
     ExpenseResponse toResponse (ExpenseModel expense);
 }
